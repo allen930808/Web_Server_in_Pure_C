a7c48c1ce23c3a49719b047a0b0d5898bf223591
allen930808 <winny930808@gmail.com>
2024-07-26 23:07:36 +0800

    Modified Cmake

--- src/showip.c
+++ src/showip.c
@@ -1,6 +1,3 @@
-//
-// Created by 陳侑廷 on 2024/7/26.
-//
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -10,38 +7,42 @@
 #include <netinet/in.h>
 
 int main(int argc, char** argv) {
-    struct addrinfo hints, *res, *p;
-    int status;
-    char ipstr[INET6_ADDRSTRLEN];
-    if(argc!=2){
-        fprintf(stderr,"usage: showip hostname\n");
+    if (argc != 2) {
+        fprintf(stderr, "usage: showip hostname\n");
         return 1;
     }
+
+    char inputstr[INET6_ADDRSTRLEN];
+    struct addrinfo hints, *res, *p;
+    int status;
+
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM;
-    if((status = getaddrinfo(argv[1], NULL, &hints, &res))) {
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return 2;
     }
+
     printf("IP addresses for %s:\n", argv[1]);
-    p = res;
-    while(p) {
+
+    for (p = res; p != NULL; p = p->ai_next) {
         void* addr;
         char* ipver;
 
-        if(p -> ai_family == AF_INET) {
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *) p -> ai_addr;
-            addr = &(ipv4 -> sin_addr);
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in* ipv4 = (struct sockaddr_in*) p->ai_addr;
+            addr = &(ipv4->sin_addr);
             ipver = "IPv4";
         } else {
-            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*) p -> ai_addr;
-            addr = &(ipv6 -> sin6_addr);
+            struct sockaddr_in6* ipv6 = (struct sockaddr_in6*) p->ai_addr;
+            addr = &(ipv6->sin6_addr);
             ipver = "IPv6";
         }
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("ip1 :%s: %s\n", ipver, ipstr);
-        p = p -> ai_next;
+
+        inet_ntop(p->ai_family, addr, inputstr, sizeof(inputstr));
+        printf("%s: %s\n", ipver, inputstr);
     }
 
     freeaddrinfo(res);
