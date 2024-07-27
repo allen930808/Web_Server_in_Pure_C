bdbd6b002f7e7b4264d507b23067f38a059660b5
allen930808 <winny930808@gmail.com>
2024-07-27 14:20:30 +0800

    Understand the basic procedure of receiving and sending message

--- include/server.h
+++ include/server.h
@@ -19,8 +19,8 @@
 #include <fcntl.h>
 #include <sys/uio.h>
 #include <arpa/inet.h>
-#define SERVER_PORT "8080"
-#define SERVERIP "127.0.0.1" //temporary
+#define PORT "8080"
 #define MAXLINE 2056
+#define BACKLOG 10
 int create_socket(void);
 #endif //SERVER_H
--- src/server.c
+++ src/server.c
@@ -1,17 +1,20 @@
 #include "../include/server.h"
 int main(void) {
-    int status;
-    struct addrinfo hint;
-    struct addrinfo *serverinfo;
+    struct addrinfo hint, *res;
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size = sizeof(client_addr);
     memset(&hint, 0, sizeof(hint));
-    hint.ai_family = AF_UNSPEC;
+    hint.ai_family = AF_INET;
     hint.ai_socktype = SOCK_STREAM;
     hint.ai_flags = AI_PASSIVE;
-    if((status = getaddrinfo(NULL, SERVER_PORT, &hint, &serverinfo)) != 0) {
-        perror("getaddrinfo error");
-        exit(-1);
-    }
+    getaddrinfo(NULL, PORT, &hint, &res);
+    const int socketfd = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
+    bind(socketfd, res -> ai_addr, res -> ai_addrlen);
+    listen(socketfd, BACKLOG);
+    int new_fd = accept(socketfd, (struct sockaddr* )&client_addr, &addr_size);
+    char* msg = "hi there";
+    int len = strlen(msg);
+    int byte_sent;
+    byte_sent = send (new_fd, msg,len, 0 )
 
-    freeaddrinfo(serverinfo);
-    return 0;
 }
