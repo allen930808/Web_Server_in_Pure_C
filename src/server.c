e92cb11cb7a63d2bf439252674a47141e85b2419
allen930808 <winny930808@gmail.com>
2024-07-27 15:25:54 +0800

    Error checking and reformatting

--- src/server.c
+++ src/server.c
@@ -1,20 +1,70 @@
 #include "../include/server.h"
 int main(void) {
-    struct addrinfo hint, *res;
+    struct addrinfo hints, *res;
     struct sockaddr_storage client_addr;
     socklen_t addr_size = sizeof(client_addr);
-    memset(&hint, 0, sizeof(hint));
-    hint.ai_family = AF_INET;
-    hint.ai_socktype = SOCK_STREAM;
-    hint.ai_flags = AI_PASSIVE;
-    getaddrinfo(NULL, PORT, &hint, &res);
-    const int socketfd = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
-    bind(socketfd, res -> ai_addr, res -> ai_addrlen);
-    listen(socketfd, BACKLOG);
-    int new_fd = accept(socketfd, (struct sockaddr* )&client_addr, &addr_size);
-    char* msg = "hi there";
+    int socketfd, new_fd;
+    char *msg = "hi there";
     int len = strlen(msg);
     int byte_sent;
-    byte_sent = send (new_fd, msg,len, 0 );
 
+    // Setup the hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Get address information
+    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
+        perror("getaddrinfo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create socket
+    socketfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (socketfd == -1) {
+        perror("socket");
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind socket
+    if (bind(socketfd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("bind");
+        close(socketfd);
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(res); // Free the address information
+
+    // Listen for connections
+    if (listen(socketfd, BACKLOG) == -1) {
+        perror("listen");
+        close(socketfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept connection
+    new_fd = accept(socketfd, (struct sockaddr *)&client_addr, &addr_size);
+    if (new_fd == -1) {
+        perror("accept");
+        close(socketfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Send message
+    byte_sent = send(new_fd, msg, len, 0);
+    if (byte_sent == -1) {
+        perror("send");
+        close(new_fd);
+        close(socketfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Close sockets
+    close(new_fd);
+    close(socketfd);
+
+    return 0;
 }
